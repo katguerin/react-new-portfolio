553cd77c338578e3baf1b99b3eded27e2b66bf5e
katguerin <katguerin55@gmail.com>
2020-08-13 16:56:24 -0700

    need to make more changes - but it's time to turn it in. i have to move on to the group project.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,10 @@ function About() {
   return (
     <section className="my-5">
       <h1 id="about">Who am I?</h1>
-      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <img src={coverImage} className="my-2" style={{ width: "60%" }} alt="cover" />
+      <p>
+      I think now that we’re in 2020 we can stop villainizing the witch from Hänsel and Gretel. Some kids ate her house. She gets to eat them. It was a fair deal.
+      </p>
     </section>
   );
 }
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 
 const Modal = ({onClose, currentPhoto}) => {
-  const { name, category, description, index } = currentPhoto;
+  const { name, category, description, image } = currentPhoto;
 
   return (
     // JSX
     <div className="modalBackdrop">
       <div className="modalContainer">
         <h3 className="modalTitle">{name}</h3>
-        <img src={require(`../../assets/photos/${category}/${index}.jpg`)} alt="current category" />
+        <img src={require(`../../assets/photos/${category}/${image.fileName}`)} alt="current category" />
         <p>{description}</p>
         <button onClick={onClose} type="button">Close this modal</button>
       </div>
